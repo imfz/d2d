79b1f087bc0ce70673764d603027829a59082ce7
Kirill Afanasjev <kirils.afanasjevs@gmail.com>
2012-12-24 17:07:55 +0200

    Minor cleanup and refactoring in minimap engine. No more clicks outside tha map.

--- dune-client/resources/js/minimapengine.js
+++ dune-client/resources/js/minimapengine.js
@@ -6,33 +6,51 @@ function MinimapGameEngine() {
 
 MinimapGameEngine.prototype.setCanvas = function (canvas) {
     this.canvas = canvas;
-}
+};
 
 MinimapGameEngine.prototype.setMap = function (map) {
     this.map = map;
-}
+};
 
 MinimapGameEngine.prototype.setEngine = function (engine) {
     this.engine = engine;
-}
+};
 
 MinimapGameEngine.prototype.bindEvents = function() {
     var that = this;
     $(this.canvas).mousedown(function(e){
+        var x = Math.floor((e.pageX-$(that.canvas).offset().left));
+        var y = Math.floor((e.pageY-$(that.canvas).offset().top));
+        var mapX = Math.round(x / that.canvas.width * that.map.getWidth() - that.engine.widthInTiles / 2);
+        var mapY = Math.round(y / that.canvas.height * that.map.getHeight() - that.engine.heightInTiles / 2);
+        if (mapX < 0) {
+            mapX = 0;
+        }
+        if (mapY < 0) {
+            mapY = 0;
+        }
+        if (mapX >= that.map.width) {
+            mapX = that.map.width-1;
+        }
+        if (mapY >= that.map.height) {
+            mapY = that.map.height-1;
+        }
+        console.log("map width" + that.map.width);
+        console.log("map height" + that.map.height);
         switch (event.which) {
         case 1:
-            var x = Math.floor((e.pageX-$(that.canvas).offset().left));
-            var y = Math.floor((e.pageY-$(that.canvas).offset().top));
-            var mapX = x / that.canvas.width * that.map.getWidth() - that.engine.widthInTiles / 2;
-            var mapY = y / that.canvas.height * that.map.getHeight() - that.engine.heightInTiles / 2;
-            that.engine.x = Math.round(mapX);
-            that.engine.y = Math.round(mapY);
+            if (mapX >= that.map.width - that.engine.widthInTiles) {
+                mapX = that.map.width - that.engine.widthInTiles;
+            }
+            if (mapY >= that.map.height - that.engine.heightInTiles) {
+                mapY = that.map.height - that.engine.heightInTiles;
+            }
+            console.log("mapX "+ mapX);
+            console.log("mapY "+ mapY);
+            that.engine.x = mapX;
+            that.engine.y = mapY;
             break;
         case 3:
-            var x = Math.floor((e.pageX-$(that.canvas).offset().left));
-            var y = Math.floor((e.pageY-$(that.canvas).offset().top));
-            var mapX = x / that.canvas.width * that.map.getWidth() - that.engine.widthInTiles / 2;
-            var mapY = y / that.canvas.height * that.map.getHeight() - that.engine.heightInTiles / 2;
             connection.sendUnitAction(that.engine.selectedUnitId,Math.round(mapX),Math.round(mapY));
             break;
         }
@@ -41,91 +59,100 @@ MinimapGameEngine.prototype.bindEvents = function() {
     $(this.canvas).bind("contextmenu",function(e){
         return false;
     });
-}
+};
 
-function putpixel(imgd, ix, iy, rd, gr, bl, al, height, pix)
-{
-    var p = (height * iy + ix) * 4;
-    pix[p]   = rd % 256; // red
-    pix[p+1] = gr % 256; // green
-    pix[p+2] = bl % 256; // blue
-    pix[p+3] = al % 256; // alpha
-}
 
 MinimapGameEngine.prototype.render = function () {
     if (this.startTime == 0) {
         this.startTime = new Date().getTime();
     }
-    var currentTime = new Date().getTime();
     this.frameCount++;
-    var fps = this.frameCount / (currentTime - this.startTime) * 1000;
-    context = this.canvas.getContext("2d");
-    context.clearRect(0, 0, this.canvas.width, canvas.height);
+    var canvas = this.canvas;
+    var context = canvas.getContext("2d");
+    var map = this.map;
+    var engine = this.engine;
 
-    var imgd = context.createImageData(this.canvas.width, this.canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var imgd = context.createImageData(canvas.width, canvas.height);
     var pix = imgd.data;
-    for (var x = 0; x < this.canvas.width; x++) {
-        for (var y = 0; y < this.canvas.height; y++) {
-            var mapX = Math.round(x / this.canvas.width * this.map.getWidth());
-            var mapY = Math.round(y / this.canvas.height * this.map.getHeight());
-            var tileType = this.map.getTileType(mapX, mapY);
-            if (tileType == TILE_TYPE_SAND) {
-                putpixel(imgd, x,y, 214, 179, 103, 255, this.canvas.height, pix);
-            }
-            else if (tileType == TILE_TYPE_ROCK) {
-                putpixel(imgd, x,y, 124, 89, 23, 255, this.canvas.height, pix);
-            }
-            else if (tileType == TILE_TYPE_SPICE) {
-                putpixel(imgd, x,y, 200, 134, 61, 255, this.canvas.height, pix);
-            }
-            else if (tileType == TILE_TYPE_RICH_SPICE) {
-                putpixel(imgd, x,y, 200, 134, 61, 255, this.canvas.height, pix);
-            } else {
-                putpixel(imgd, x,y, 0,0,0, 255, this.canvas.height, pix);
-            }
 
-        }
-    }
-
-    for (var i = 0; i < this.map.units.length; i++) {
-        var unit = this.map.units[i];
-        var x = Math.round(unit.x / this.map.getWidth() * this.canvas.width );
-        var y = Math.round(unit.y / this.map.getHeight() * this.canvas.height);
-        var color = sprites.getPlayerColor(unit.ownerId);
-
-        putpixel(imgd, x,y, color.r, color.g, color.b, 255, this.canvas.height, pix);
-    }
-
-    for (var i = 0; i < this.map.buildings.length; i++) {
-        var building = this.map.buildings[i];
-        var x = Math.round(building.x / this.map.getWidth() * this.canvas.width );
-        var y = Math.round(building.y / this.map.getHeight() * this.canvas.height);
-        for (var w = x; w < x + building.width; w++) {
-            for (var h = y; h < y + building.height; h++) {
-                var color = sprites.getPlayerColor(building.ownerId);
-                putpixel(imgd, w,h, color.r, color.g, color.b, 255, this.canvas.height, pix);
-            }
-        }
-
-    }
-
-    var x1 = this.engine.x * 1.0 / this.map.getWidth() * this.canvas.width;
-    var y1 = this.engine.y * 1.0 / this.map.getHeight() * this.canvas.height;
-
-    var x2 = (this.engine.x * 1.0 + this.engine.widthInTiles) / this.map.getWidth() * this.canvas.width;
-    var y2 = (this.engine.y * 1.0 + this.engine.heightInTiles) / this.map.getHeight() * this.canvas.height;
-
-    context.putImageData(imgd, 0,0);
-
-    context.strokeStyle = "#000000";
-    context.lineWidth = 2;
-    context.strokeRect(x1,y1,x2-x1,y2-y1);
-
+    drawTerrain();
+    drawUnits();
+    drawBuildings();
+    drawRectangle();
     var that = this;
     setTimeout(function () {
         that.render()
     }, 1000 / 60);
-}
+
+    function putpixel(imgd, ix, iy, rd, gr, bl) {
+        var p = (canvas.height * iy + ix) * 4;
+        pix[p]   = rd % 256; // red
+        pix[p+1] = gr % 256; // green
+        pix[p+2] = bl % 256; // blue
+        pix[p+3] = 255; // alpha
+    }
+
+    function drawTerrain() {
+        for (var x = 0; x < canvas.width; x++) {
+            for (var y = 0; y < canvas.height; y++) {
+                var mapX = Math.round(x / canvas.width * map.getWidth());
+                var mapY = Math.round(y / canvas.height * map.getHeight());
+                var tileType = map.getTileType(mapX, mapY);
+                if (tileType == TILE_TYPE_SAND) {
+                    putpixel(imgd, x, y, 214, 179, 103);
+                }
+                else if (tileType == TILE_TYPE_ROCK) {
+                    putpixel(imgd, x, y, 124, 89, 23);
+                }
+                else if (tileType == TILE_TYPE_SPICE) {
+                    putpixel(imgd, x, y, 200, 134, 61);
+                }
+                else if (tileType == TILE_TYPE_RICH_SPICE) {
+                    putpixel(imgd, x, y, 200, 134, 61);
+                } else {
+                    putpixel(imgd, x, y, 0, 0, 0);
+                }
+            }
+        }
+    }
+
+    function drawUnits() {
+        for (var i = 0; i < map.units.length; i++) {
+            var unit = map.units[i];
+            var x = Math.round(unit.x / map.getWidth() * canvas.width);
+            var y = Math.round(unit.y / map.getHeight() * canvas.height);
+            var color = sprites.getPlayerColor(unit.ownerId);
+            putpixel(imgd, x, y, color.r, color.g, color.b, 255, canvas.height, pix);
+        }
+    }
+
+    function drawBuildings() {
+        for (var i = 0; i < map.buildings.length; i++) {
+            var building = map.buildings[i];
+            var x = Math.round(building.x / map.getWidth() * canvas.width);
+            var y = Math.round(building.y / map.getHeight() * canvas.height);
+            for (var w = x; w < x + building.width; w++) {
+                for (var h = y; h < y + building.height; h++) {
+                    var color = sprites.getPlayerColor(building.ownerId);
+                    putpixel(imgd, w, h, color.r, color.g, color.b, 255, canvas.height, pix);
+                }
+            }
+        }
+    }
+
+    function drawRectangle() {
+        var x1 = engine.x * 1.0 / map.getWidth() * canvas.width;
+        var y1 = engine.y * 1.0 / map.getHeight() * canvas.height;
+        var x2 = (engine.x * 1.0 + engine.widthInTiles) / map.getWidth() * canvas.width;
+        var y2 = (engine.y * 1.0 + engine.heightInTiles) / map.getHeight() * canvas.height;
+        context.putImageData(imgd, 0, 0);
+        context.strokeStyle = "#000000";
+        context.lineWidth = 2;
+        context.strokeRect(x1, y1, x2 - x1, y2 - y1);
+    }
+
+};
 
 
 
